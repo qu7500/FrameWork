fad1ff8f7fa9049e5d705e2921b637d290e69ddb
bluewing <bluewing@a58f6b97-8f21-468a-8775-5c93c435eee4>
2018-03-03 09:24:31 +0000

    qudaio

---
+++ UI/Components/ScaleScrollRect/ScaleScrollItemBase.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleScrollItemBase : UIBase 
+{
+    public virtual void SetData(Dictionary<string,object> data)
+    {
+
+    }
+
+    public virtual void Dispose()
+    {
+
+    }
+
+    /// <summary>
+    /// 被选中调用
+    /// </summary>
+    public  virtual void OnSelect()
+    {
+
+    }
+
+    /// <summary>
+    /// 取消选中调用
+    /// </summary>
+    public virtual void OnUnSelect()
+    {
+
+    }
+}
